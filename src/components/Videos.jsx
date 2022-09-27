8879658cc374917b506701100aa345e179822a64
odedmasala <odedmasala6@gmail.com>
2022-09-27 23:21:17 +0300

    finsed project

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
-import { ChannelCard, VideoCard } from "./";
-const Videos = ({videos}) => {
-console.log(videos);
+
+import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return <Loader />;
+  
   return (
-     <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
-      {videos[0] && videos[0].map((item, idx) => (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} /> }
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
-  )
+  );
 }
 
-export default Videos
+export default Videos;
